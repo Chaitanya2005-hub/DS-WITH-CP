100873624880496da9394c638352f2d05e46769a
Chaitanya2005-hub <241801120002@cutmap.ac.in>
2025-12-18 19:17:32 +0530

    Update task.java

--- module1_Array/task.java
+++ module1_Array/task.java
@@ -1,6 +1,3 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
-
 class Main {
     public static void main(String[] args) {
         
@@ -28,9 +25,6 @@ class Main {
         }
         
         
-        // Dynamic
-        
-        int [10] = 
         
         
     }
